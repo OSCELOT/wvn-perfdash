8f23747ac264e10800e22b725ac3d39c9ef4057c
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-06-16 16:43:08 -0400

    strip out spring stuff

--- src/main/java/edu/wvnet/earlywarning/CheckPermissions.java
+++ src/main/java/edu/wvnet/earlywarning/CheckPermissions.java
@@ -2,27 +2,17 @@ package edu.wvnet.earlywarning;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-//import java.sql.ResultSetMetaData;
 import java.sql.PreparedStatement;
-import blackboard.db.ConnectionManager;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import blackboard.db.ConnectionManager;
 import blackboard.platform.session.BbSession;
 import blackboard.platform.session.BbSessionManagerService;
 import blackboard.platform.session.BbSessionManagerServiceFactory;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.ModelMap;
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class CheckPermissions {
 
-	@RequestMapping( value = { "checkPermissions" }, method = RequestMethod.GET )
-	public String getPage( HttpServletRequest request, HttpServletResponse response, ModelMap model ) {
+	public String getPage( HttpServletRequest request, HttpServletResponse response ) {
 		BbSessionManagerService sessman = BbSessionManagerServiceFactory.getInstance();
 		BbSession sess = sessman.getSession(request);
 		if(!sess.isAuthenticated()) return "error";
--- src/main/java/edu/wvnet/earlywarning/CourseDisplay.java
+++ src/main/java/edu/wvnet/earlywarning/CourseDisplay.java
@@ -1,28 +1,20 @@
 package edu.wvnet.earlywarning;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.ModelMap;
-import org.springframework.stereotype.Controller;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
-//import java.sql.ResultSetMetaData;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import blackboard.db.ConnectionManager;
 import blackboard.db.ConnectionNotAvailableException;
 import blackboard.platform.session.BbSession;
 import blackboard.platform.session.BbSessionManagerService;
 import blackboard.platform.session.BbSessionManagerServiceFactory;
 
-@Controller
 public class CourseDisplay {
 
-	@RequestMapping( value = { "courseDisplay" }, method = RequestMethod.GET )
-	public String getPage( HttpServletRequest request, HttpServletResponse response, ModelMap model ) {
+	public String getPage( HttpServletRequest request, HttpServletResponse response ) {
 		BbSessionManagerService sessman = BbSessionManagerServiceFactory.getInstance();
 		BbSession sess = sessman.getSession(request);
 		if(!sess.isAuthenticated()) return "error";
