dd8b8a65d2f86839cecd79bde84f1f3cce85584c
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-06-20 16:49:14 -0400

    work in progress for RenderingHook

--- src/main/java/edu/wvnet/earlywarning/EWSRenderingHook.java
+++ src/main/java/edu/wvnet/earlywarning/EWSRenderingHook.java
@@ -6,12 +6,13 @@ public class EWSRenderingHook implements RenderingHook {
 
 	@Override
 	public String getContent() {
-		return "tag.learningSystemPage.start";
+		return "jsp.topFrame.start";
 	}
 
 	@Override
 	public String getKey() {
-		return "<script type='text/javascript' src='/webapps/wvn-earlywarning-BBTSTLRN/renderinghook.js'></script>";
+		//return "<script type='text/javascript' src='/webapps/wvn-earlywarning-BBTSTLRN/renderinghook.js'></script>";
+		return "hey hey hey here is my hook lol";
 	}
 
 }
