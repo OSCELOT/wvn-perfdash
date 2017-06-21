1278d3747f066d0ed66a8163ad0f6b11eda7f4a0
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-06-21 13:22:39 -0400

    rename EWSRenderingHook to TopFrameRenderingHook, clean up bb-manifest

--- src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
+++ src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
@@ -2,7 +2,7 @@ package edu.wvnet.earlywarning;
 
 import blackboard.servlet.renderinghook.RenderingHook;
 
-public class EWSRenderingHook implements RenderingHook {
+public class TopFrameRenderingHook implements RenderingHook {
 
 	@Override
 	public String getContent() {
@@ -12,7 +12,7 @@ public class EWSRenderingHook implements RenderingHook {
 	@Override
 	public String getKey() {
 		//return "<script type='text/javascript' src='/webapps/wvn-earlywarning-BBTSTLRN/renderinghook.js'></script>";
-		return "hey hey hey here is my hook lol";
+		return "<div>hey hey hey here is my hook lol</div>";
 	}
 
 }
