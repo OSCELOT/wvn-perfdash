debbb11304cde7c5246aceb5bcff47c688ad7f2c
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-06-22 10:59:28 -0400

    RenderingHook actually works now

--- src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
+++ src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
@@ -5,14 +5,14 @@ import blackboard.servlet.renderinghook.RenderingHook;
 public class TopFrameRenderingHook implements RenderingHook {
 
 	@Override
-	public String getContent() {
+	public String getKey() {
 		return "jsp.topFrame.start";
 	}
-
+	
 	@Override
-	public String getKey() {
+	public String getContent() {
 		//return "<script type='text/javascript' src='/webapps/wvn-earlywarning-BBTSTLRN/renderinghook.js'></script>";
-		return "<div>hey hey hey here is my hook lol</div>";
+		return "hey hey here is my hook lol";
 	}
 
 }
