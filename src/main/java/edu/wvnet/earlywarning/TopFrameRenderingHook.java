70adc9f994b53598cc1ca9aed6c00b5f1f39da21
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-06-23 16:39:32 -0400

    comments for clarity

--- src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
+++ src/main/java/edu/wvnet/earlywarning/TopFrameRenderingHook.java
@@ -11,7 +11,10 @@ public class TopFrameRenderingHook implements RenderingHook {
 	}
 	
 	@Override
-	public String getContent() {		
+	public String getContent() {
+		// in order to minimize the amount of multi-line string work I have to do
+		// instead I'm using this convoluted solution with tons of back and fourth
+		// don't judge me
 		return "<!-- Hide WVNET Early Warning System from users without access -->\n"
 		        + "<script type='text/javascript'>\n"
 		        + "function checkEWSAccess(callback) {\n"
