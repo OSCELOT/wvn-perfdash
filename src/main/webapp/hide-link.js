f6b20ff49f5863170898cdc3b76cb77f42f9f918
Jonathan Lynch <jonathan@mail.wvnet.edu>
2017-08-31 11:57:38 -0400

    module is now hidden for students the same way the Tools link is

--- src/main/webapp/hide-link.js
+++ src/main/webapp/hide-link.js
@@ -1,11 +1,18 @@
 function hideSPDLink() {
 	var modules = document.getElementsByClassName("moduleTitle");
+	// remove tools link if present
 	for (var i = 0; i < modules.length; i++) if(modules[i].innerHTML == "Tools") var tools = modules[i];
 	if(tools) {
 		tools = tools.parentElement.parentElement.getElementsByClassName("portletList");
 		tools[0].childElements().forEach(
 			function (item) { if(item.innerHTML.includes("wvn-perfdash")) item.remove() }
 		)
+	}
+	// remove module if present
+	for (var i = 0; i < modules.length; i++) if(modules[i].innerHTML == "Student Performance Dashboard") var spd = modules[i];
+	if(spd) {
+		spd = spd.parentElement.parentElement;
+		spd.remove();
 	}
 }
 
